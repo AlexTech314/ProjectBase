f04e82268a1c2d56242085f3d5b1a4cf7f6d243f
AlexTech314 <alest314@gmail.com>
2024-11-13 21:53:04 -0500

    Trying to add support for static files...

--- ui/index.js
+++ ui/index.js
@@ -1,16 +1,18 @@
-const serverless = require('serverless-http');
+const { parse } = require('url');
 const next = require('next');
+const serverless = require('serverless-http');
 
-const app = next({
-  dev: false,
-  conf: {
-    // Any custom Next.js configuration goes here
-  },
-});
-
+const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 exports.handler = async (event, context) => {
+  const parsedUrl = parse(event.rawPath, true);
+  const { pathname } = parsedUrl;
+
+  if (pathname.startsWith('/_next') || pathname.startsWith('/static')) {
+    return serverless((req, res) => handle(req, res))(event, context);
+  }
+
   await app.prepare();
   return serverless((req, res) => handle(req, res))(event, context);
 };
