3e9f3ebd1b30a7516dcb709b09d7868e7477e647
AlexTech314 <alest314@gmail.com>
2024-11-13 21:24:45 -0500

    Updating express invokation

--- ui/index.js
+++ ui/index.js
@@ -3,9 +3,4 @@ const path = require('path');
 
 const app = require(path.join(__dirname, '.next/standalone/server.js'));
 
-const handle = app.getRequestHandler();
-
-exports.handler = async (event, context) => {
-  await app.prepare();
-  return serverless((req, res) => handle(req, res))(event, context);
-};
+exports.handler = serverless(app);
