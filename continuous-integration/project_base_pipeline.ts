ea92943730c20bbc085141629a6b6940ed21ef4c
AlexTech314 <alest314@gmail.com>
2024-11-15 18:49:54 -0500

    Add this to github

--- continuous-integration/project_base_pipeline.ts
+++ continuous-integration/project_base_pipeline.ts
@@ -12,6 +12,7 @@ export class ProjectBasePipeline extends cdk.Stack {
             synth: new ShellStep('Synth', {
                 input: CodePipelineSource.gitHub('AlexTech314/ProjectBase', 'main'),
                 commands: [
+                    'cd ui && npm install',
                     'npm ci',
                     'npm run build',
                     'npx cdk synth'
