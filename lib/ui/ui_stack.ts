d79c9662130fdb05a44bd7323bbc227bf72c5a3c
AlexTech314 <alest314@gmail.com>
2024-11-14 19:49:24 -0500

    Getting rid of the bad lambda.

--- lib/ui/ui_stack.ts
+++ lib/ui/ui_stack.ts
@@ -17,36 +17,36 @@ export class UIStack extends cdk.NestedStack {
     const { vpc } = props;
 
     // Create the Lambda function using DockerImageFunction
-    const lambdaFunction = new DockerImageFunction(this, 'NextJsLambdaFunction', {
-      code: DockerImageCode.fromImageAsset('./ui', {
-        // Exclude unnecessary files to reduce image size
-        exclude: ['cdk.out', 'node_modules', '.git', '.github', '.vscode'],
-      }),
-      vpc,
-      vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
-      memorySize: 1024, // Adjust as needed
-      timeout: cdk.Duration.seconds(30), // Adjust as needed
-    });
+    // const lambdaFunction = new DockerImageFunction(this, 'NextJsLambdaFunction', {
+    //   code: DockerImageCode.fromImageAsset('./ui', {
+    //     // Exclude unnecessary files to reduce image size
+    //     exclude: ['cdk.out', 'node_modules', '.git', '.github', '.vscode', '.next'],
+    //   }),
+    //   vpc,
+    //   vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+    //   memorySize: 1024, // Adjust as needed
+    //   timeout: cdk.Duration.seconds(30), // Adjust as needed
+    // });
 
-    // Create API Gateway and integrate it with the Lambda function
-    const api = new LambdaRestApi(this, 'NextJsApiGateway', {
-      handler: lambdaFunction,
-      proxy: true,
-      defaultCorsPreflightOptions: {
-        allowOrigins: ['*'],
-        allowMethods: ['*'],
-        allowHeaders: ['*'],
-        allowCredentials: true,
-        maxAge: cdk.Duration.days(1)
-      } 
-    });
+    // // Create API Gateway and integrate it with the Lambda function
+    // const api = new LambdaRestApi(this, 'NextJsApiGateway', {
+    //   handler: lambdaFunction,
+    //   proxy: true,
+    //   defaultCorsPreflightOptions: {
+    //     allowOrigins: ['*'],
+    //     allowMethods: ['*'],
+    //     allowHeaders: ['*'],
+    //     allowCredentials: true,
+    //     maxAge: cdk.Duration.days(1)
+    //   } 
+    // });
 
-    this.apiUrl = api.url;
+    // this.apiUrl = api.url;
 
-    // Output the API URL
-    new cdk.CfnOutput(this, 'ApiUrl', {
-      value: this.apiUrl,
-      description: 'API Gateway endpoint URL',
-    });
+    // // Output the API URL
+    // new cdk.CfnOutput(this, 'ApiUrl', {
+    //   value: this.apiUrl,
+    //   description: 'API Gateway endpoint URL',
+    // });
   }
 }
