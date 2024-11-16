d87fe4d6316b78ffe3ea665ce91e9403127a3c6c
AlexTech314 <alest314@gmail.com>
2024-11-15 21:26:39 -0500

    Tough stuff here.

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import React from "react";
 
 export default function Home() {
   return (
@@ -6,7 +7,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="static/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -58,7 +59,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="static/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -73,7 +74,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="static/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -88,12 +89,12 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="static/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to nextjs.org!
         </a>
       </footer>
     </div>
