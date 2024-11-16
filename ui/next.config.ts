039dca8a0c74bbec7134206a848dd648ca7a98ac
AlexTech314 <alest314@gmail.com>
2024-11-15 20:28:06 -0500

    Fixing worspace issue

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -3,9 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   poweredByHeader: false,
   cleanDistDir: true,
-  transpilePackages: ["@example/shared"],
   output: "standalone",
-  // outputFileTracingRoot: "../sst",
   eslint: {
     ignoreDuringBuilds: true,
   },
