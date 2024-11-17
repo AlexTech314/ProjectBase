a7e7e324a10d63705d6d06bbc4f78f33d09fb230
AlexTech314 <alest314@gmail.com>
2024-11-16 20:02:01 -0500

    I dont think this will work, but lets try

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_MAIN_API_URL: "https://3c5pp045vl.execute-api.us-west-1.amazonaws.com/prod"
+  }
 };
 
 export default nextConfig;
