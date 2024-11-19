d7dd37786a12ecc783909ab7d4ecf43f38ed0fa3
AlexTech314 <alest314@gmail.com>
2024-11-18 20:20:05 -0500

    Wrong api

--- ui/next.config.ts
+++ ui/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   env: {
-    NEXT_PUBLIC_MAIN_API_URL: "https://3c5pp045vl.execute-api.us-west-1.amazonaws.com/prod"
+    NEXT_PUBLIC_MAIN_API_URL: "https://p2rjlhaqrf.execute-api.us-west-1.amazonaws.com/prod"
   }
 };
 
