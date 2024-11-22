de52122c0d0fe5655a32511a124bc537f08062c4
AlexTech314 <alest314@gmail.com>
2024-11-22 16:13:18 -0500

    Updating changelog path

--- lib/db/relational_db.ts
+++ lib/db/relational_db.ts
@@ -99,7 +99,7 @@ export class RelationalDb extends Construct {
                             'echo Running Liquibase changelog',
                             'liquibase \
                             --classpath=/liquibase/classpath/mysql-connector-java.jar \
-                            --changeLogFile=src/db/changeLog.sql \
+                            --changeLogFile=changeLog.sql \
                             --url="jdbc:mysql://${DB_HOST}:${DB_PORT}/${DB_NAME}" \
                             --username=${DB_USER} \
                             --password=${DB_PASSWORD} \
